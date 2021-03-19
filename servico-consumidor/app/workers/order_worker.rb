115f41718f6721ea90e0a3b2c7e0c932e0dcc05c
Matheus Perrut <matheus.perrut2302@gmail.com>
2021-03-19 15:48:44 -0300

    - Alterações para a comunicação com api de endereços

--- servico-consumidor/app/workers/order_worker.rb
+++ servico-consumidor/app/workers/order_worker.rb
@@ -19,7 +19,7 @@ class OrderWorker
       order["address_attributes"]["lng"] = "lng"
 
       params = {
-        body: JSON.generate(order),
+        body: {},
         headers: { 
            "Accept" => "application/json",
            "Content-Type" => "application/json"
@@ -27,6 +27,32 @@ class OrderWorker
       }
 
       params[:debug_output] = STDOUT
+
+      formatted_address = ""
+      formatted_address += order["address_attributes"]["street"] + " "
+      formatted_address += order["address_attributes"]["neighborhood"] + " "
+      formatted_address += order["address_attributes"]["city"] + " "
+      formatted_address += order["address_attributes"]["uf"] + " "
+      formatted_address += order["address_attributes"]["zip_code"]
+
+      begin
+        response_address = HTTParty.get("http://api.positionstack.com/v1/forward?" +
+        "access_key=9c41309b6d87bd8505b2a1a6b4387488&query="+ CGI.escape("#{formatted_address}"))
+
+        if response_address.code == 200
+          begin
+            address = JSON.parse(response_address.body)
+            order["address_attributes"]["lat"] = address["data"][0]["latitude"]
+            order["address_attributes"]["lng"] = address["data"][0]["longitude"]
+          rescue
+            order["address_attributes"]["lat"] = "n/a"
+            order["address_attributes"]["lng"] = "n/a"
+          end
+        end
+      rescue
+      end
+
+      params[:body] = JSON.generate(order)
 
       HTTParty.put("http://producer:3000/orders/#{order["id"]}", params)
       ack! 
