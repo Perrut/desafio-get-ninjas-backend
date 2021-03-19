5d1b3284ca6299531b341e297a01a1591d303073
Matheus Perrut <matheus.perrut2302@gmail.com>
2021-03-19 09:28:29 -0300

    - Rodando migrations

--- servico-produtor/db/schema.rb
+++ servico-produtor/db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_004914) do
+ActiveRecord::Schema.define(version: 2021_03_19_122613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "address_attributes", force: :cascade do |t|
+    t.string "city"
+    t.string "neighborhood"
+    t.string "street"
+    t.string "uf"
+    t.string "zip_code"
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_address_attributes_on_order_id"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -22,4 +34,32 @@ ActiveRecord::Schema.define(version: 2021_03_19_004914) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "request_infos", force: :cascade do |t|
+    t.string "question1"
+    t.string "question2"
+    t.string "question3"
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_request_infos_on_order_id"
+  end
+
+  create_table "user_infos", force: :cascade do |t|
+    t.string "phone"
+    t.string "name"
+    t.string "email"
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_user_infos_on_order_id"
+  end
+
+  add_foreign_key "address_attributes", "orders"
+  add_foreign_key "request_infos", "orders"
+  add_foreign_key "user_infos", "orders"
 end
